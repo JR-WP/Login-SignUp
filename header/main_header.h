7e2bd0012832b0be8298103609b528794b419b9a
JR-WP <130521666+JR-WP@users.noreply.github.com>
2023-05-01 18:58:00 -0700

    Add files via upload

---
+++ header/main_header.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_HEADER_H_INCLUDED
+#define MAIN_HEADER_H_INCLUDED
+
+/*
+This file will contain all the needed components
+for this program to work.
+*/
+
+//Preprocessor Directives
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+
+//Linked files together
+#include "newUser.cpp"
+#include "eUser.cpp"
+#include "unAndPw_checker.cpp"
+
+
+#endif // MAIN_HEADER_H_INCLUDED
+
